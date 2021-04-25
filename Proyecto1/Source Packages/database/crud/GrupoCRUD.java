8dfa550905aa70812038f2223421b6cb6a4a64e2
crisud <crisurena17@gmail.com>
2021-04-25 17:19:43 -0600

    base de datos2

---
+++ Proyecto1/Source Packages/database/crud/GrupoCRUD.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.crud;
+
+import database.AbstractCRUD;
+
+/**
+ *
+ * @author USER
+ */
+public class GrupoCRUD extends AbstractCRUD
+{
+    @Override
+    public String getListAllCmd()
+    {
+        return LIST_CMD;
+    }
+
+    @Override
+    public String getAddCmd()
+    {
+        return ADD_CMD;
+    }
+
+    @Override
+    public String getRetrieveCmd()
+    {
+        return RETRIEVE_CMD;
+    }
+
+    @Override
+    public String getUpdateCmd()
+    {
+        return UPDATE_CMD;
+    }
+    
+    protected static final String LIST_CMD
+            = "SELECT id, id_profesor, id_curso, horario FROM proyecto1.grupos "
+            + "ORDER BY id; ";
+    protected static final String ADD_CMD
+            = "INSERT INTO proyecto1.grupos (id, id_profesor, id_curso, horario) "
+            + "VALUES (?, ?, ?, ?); "; 
+    protected static final String RETRIEVE_CMD
+            = "SELECT id, id_profesor, id_curso, horario FROM proyecto1.grupos "
+            + "WHERE id = ?; ";
+    protected static final String UPDATE_CMD // necesario??
+            = "UPDATE proyecto1.grupos SET horario = ? "
+            + "WHERE id = ?; ";
+}
